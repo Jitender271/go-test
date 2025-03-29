afeae1d5fd8e20e85a8aeeeb71d554b49750357d
jitender <jitenderkmr271@gmail.com>
2025-03-30 00:22:59 +0530

    test case file added

--- main.go
+++ main.go
@@ -1,1 +1,12 @@
 package main
+
+import "fmt"
+
+// Add adds two numbers
+func Add(a, b int) int {
+	return a + b
+}
+
+func main() {
+	fmt.Println("Hello, Go!")
+}
--- main_test.go
+++ main_test.go
@@ -1,12 +1,8 @@
-package tests
+package main
 
 import (
 	"testing"
 )
-
-func Add(a, b int) int {
-	return a + b
-}
 
 func TestAdd(t *testing.T) {
 	result := Add(2, 3)
